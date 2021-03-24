baa43bc4b14c689a72192320f709661575013ab3
YD <hoggaz@naver.com>
2021-03-24 20:16:14 +0900

    2. 로그인/회원가입 구현

--- src/main/java/com/cos/blog/domain/user/User.java
+++ src/main/java/com/cos/blog/domain/user/User.java
@@ -1,22 +1,41 @@
 package com.cos.blog.domain.user;
 
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.cos.blog.domain.auth.Auth;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
-@NoArgsConstructor
-@AllArgsConstructor
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Entity
 public class User {
-	private Integer id;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id; 
+	
 	private String phoneNumber;
-	private String nickname;
+	
+	@Column(nullable =false ,length = 50,unique=true)  //  nullable : null 허용 x , length : 글자길이 , unique : 중복허용 x 
+	private String nickName;
+	
 	private String photo;
+	
+	@CreationTimestamp
+	private Timestamp createDate;
 
 }
